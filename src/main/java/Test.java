27c8234fa9a5582097aa0e5a724b3e5f8166c0ce
tsy <309424886@qq.com>
2018-07-11 15:50:07 +0800

    第2次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,23 @@
+/**
+ * Created by lx on 2018/7/11.
+ */
+public class Test {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
